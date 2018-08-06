c646606886a384f6d2617afb32c3020084a3543c
Wyatt Ferguson <wyattf@gmail.com>
2018-08-06 15:06:46 -0400

    Initial Setup, graphics, fonts, and components

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+
+    public Text scoreText;
+    public Text endText;
+
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+}
